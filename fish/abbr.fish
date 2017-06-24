c60dd3ce56596fcfb5f808c0b519830b5add7c7f
Florian Rampp <florian.rampp@tado.com>
2017-06-24 11:02:46 +0200

    add fish config and functions

---
+++ fish/abbr.fish
@@ -0,0 +1,23 @@
+#!/usr/local/bin/fish
+
+# create/change this file with abbr --show >> abbr.fish
+
+abbr gco 'git checkout'
+abbr gst 'git status'
+abbr gpr 'hub pull-request'
+abbr gb 'git branch'
+abbr grb 'git rebase'
+abbr grbi 'git rebase --interactive'
+abbr gap 'git add -p'
+abbr gc 'git commit'
+abbr gc! 'git commit --amend'
+abbr gcm 'git checkout master'
+abbr gd 'git diff'
+abbr ga 'git add'
+abbr gdc 'git diff --cached'
+abbr grbc 'git rebase --continue'
+abbr gcp 'git cherry-pick'
+abbr grba 'git rebase --abort'
+abbr gcf 'git commit --fixup='
+
+# end
